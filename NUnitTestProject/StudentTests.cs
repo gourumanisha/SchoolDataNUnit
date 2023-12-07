30eb0bcd165b0a13d51d86eb61adfc2a5cd050e0
gourumanisha <147041130+gourumanisha@users.noreply.github.com>
2023-12-07 12:10:11 +0530

    Add project files.

---
+++ NUnitTestProject/StudentTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolDataNUnit;
+
+namespace NUnitTestProject
+{
+    public class StudentTests
+    {
+        [Test]
+        public void TestStudentName()
+        {
+            // Arrange
+            Student student = new Student();
+            student.Name = "Manisha";
+            // Act
+            string studentName = student.Name;
+            // Assert
+            Assert.AreEqual("Manisha", studentName);
+        }
+        [Test]
+        public void TestStudentAddress()
+        {
+            // Arrange
+            Student student = new Student();
+            student.Address = "123 Main St";
+            // Act
+            string studentAddress = student.Address;
+            // Assert
+            Assert.AreEqual("123 Main St", studentAddress);
+        }
+    }
+}
---
+++ NUnitTestProject/TeachersTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolDataNUnit;
+
+namespace NUnitTestProject
+{
+    [TestFixture]
+    public class TeachersTest
+    {
+        [Test]
+        public void TestTeacherName()
+        {
+            // Arrange
+            Teacher myTeacher = new Teacher();
+            myTeacher.Name = "Prathap Reddy";
+            // Act
+            string teacherName = myTeacher.Name;
+            // Assert
+            Assert.AreEqual("Prathap Reddy", teacherName);
+        }
+        [Test]
+        public void TestClassId()
+        {
+            // Arrange
+            Teacher myTeacher = new Teacher();
+            myTeacher.Id = 1;
+            // Act
+            int tecaherId = myTeacher.Id;
+            // Assert
+            Assert.AreEqual(1, tecaherId);
+        }
+    }
+}
